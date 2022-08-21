e9c6da1dc1602cfb79ce0c3ddeab8f7400e94e28
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-22 02:31:11 +0600

    Update autoloader.inc.php

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -1,11 +1,22 @@
 <?php
 
-function autoload($class) {
-  $path = '../classes/';
-  $extention = '.class.php';
-  $file = $path . $class . $extention;
-  
-  include $file;
-}
- 
 spl_autoload_register('autoload');
+
+function autoload($class) {
+  $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+  if(strpos($url, 'includes') !== false) {
+    $path = '../classes/';
+  }
+  else {
+    $path = 'classes/';
+  }
+  $extention = '.class.php';
+  $filePath = $path . strtolower($class) . $extention;
+
+  if(!file_exists($filePath)) {
+    return false;
+  }
+  
+  require_once $filePath;
+}
