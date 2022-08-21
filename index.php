743f7d3ee2dc62c3da5e81f3089c7318bbc1e18b
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-22 02:34:34 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,6 @@
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<center>
-<?php
- // Sample Content <start>
- $testObj = new Sample;
- echo $testObj->getContent();
- //Sample Content <end>
-?>
-</center>
 
 </body>
 </html>
