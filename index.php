4c942a2f152cc8218810d01e75f62b1e84083cce
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-22 02:30:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
- require "autoloader.inc.php";
+ declare(strict_types = 1);
+ require "includes/autoloader.inc.php";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sample Title</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<center>
+<?php
+ // Sample Content <start>
+ $testObj = new Sample;
+ echo $testObj->getContent();
+ //Sample Content <end>
+?>
+</center>
+
+</body>
+</html>
