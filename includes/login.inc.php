8682f85a18ed5f364ffaf68e4b6526f27971523e
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-22 23:16:47 +0600

    Create login.inc.php

---
+++ includes/login.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])) {
+  // Grabbing Values
+  
+  // Instantiate Classes
+  include '../classes/login.class.php';
+  include '../classes/logincontr.class.php';
+  $signup = new LoginContr();
+  
+  // Running error handlers and user signup
+  
+  // Redirecting to front page
+   
+}
