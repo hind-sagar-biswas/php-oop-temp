24a010c546f4fea45b15edfabc5322f98a19662d
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-22 23:15:58 +0600

    Create signup.inc.php

---
+++ includes/signup.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])) {
+  // Grabbing Values
+  
+  // Instantiate Classes
+  include '../classes/signup.class.php';
+  include '../classes/signupcontr.class.php';
+  $signup = new SignupContr();
+  
+  // Running error handlers and user signup
+  
+  // Redirecting to front page
+   
+}
