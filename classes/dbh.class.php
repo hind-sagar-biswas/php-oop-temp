a5ed06e211d80240fefe438c5e90dea3e0ebffb6
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-24 17:53:14 +0600

    Update dbh.class.php

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -18,8 +18,20 @@ class Dbh {
   public function setPort(int $port) {
     $this->port = $port;
   }
-
+  
   protected function connect() {
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    try {
+      $mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db);
+      $mysqli->set_charset("utf8mb4");
+    }
+    catch(Exception $e) {
+      error_log($e->getMessage());
+      exit('Error connecting to database');
+    }
+  }
+
+  protected function connect_pdo() {
     try {
       $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db;
       $pdo = new PDO($dsn, $this->user, $this->pass);
